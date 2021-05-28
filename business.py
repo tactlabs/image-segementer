2acdab0a1a57ae904fe17df9c0b2515f4870db52
Talha Abdur Rahman <cooltalha27@gmail.com>
2021-05-29 01:51:47 +0530

    Final Image Saved

--- business.py
+++ business.py
@@ -23,7 +23,7 @@ def add_background(img_url,bg_url):
     rgb_img = img.convert('RGB')
     new_image_resized = rgb_img.resize((224, 224))
 
-    # new_image_resized.save('image_resized1.png')
+    new_image_resized.save('image_resized1.png')
 
     img_resize = Image.open('image_resized1.png')
     plt.imshow(img_resize)
@@ -113,7 +113,7 @@ def add_background(img_url,bg_url):
     # plt.show()
 
     background1 = Image.fromarray(blur_rgb)
-    # background1.save("space_background.png")
+    background1.save("space_background.png")
 
     background_image = cv2.imread('space_background.png')
     background_image = cv2.cvtColor(background_image, cv2.COLOR_BGR2RGB)
@@ -134,7 +134,7 @@ def add_background(img_url,bg_url):
     rgb_new = new_op.convert('RGB')
     new_image_bg = rgb_new.resize((224, 224))
 
-    # new_image_bg.save('image_resized_bg.png')
+    new_image_bg.save('image_resized_bg.png')
 
     background_image_new = cv2.imread('image_resized_bg.png')
     background_image_new = cv2.cvtColor(background_image_new, cv2.COLOR_BGR2RGB)
@@ -144,8 +144,11 @@ def add_background(img_url,bg_url):
     plt.imshow(crop_background_new)
 
     complete_image_newbg = masked_image + crop_background_new
-    plt.imshow(complete_image_newbg)
-    plt.show()
+    # complete_image_newbg.save('complete_image_newbg.png')
+    complete_image_newbg = cv2.cvtColor(complete_image_newbg, cv2.COLOR_BGR2RGB)
+    cv2.imwrite('complete_image_newbg.png',complete_image_newbg)
+    # plt.imshow(complete_image_newbg)
+    # plt.show()
 
 
 if __name__ == "__main__":
